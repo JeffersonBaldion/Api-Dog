2438dfde927214be27b40f2bee6bb6e8d9ad68b5
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2023-09-05 19:53:53 -0500

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,28 +1,34 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-const DogModel = require('./models/Dog')
-const TemperamentModel = require('./models/Temperaments')
+const DogModel = require("./models/Dog");
+const TemperamentModel = require("./models/Temperaments");
 
 const sequelize = new Sequelize(
-   
-    `postgres://dogdatabase_0vag_user:qoypINbBwAMs59w2b0Q4glywuKpWbeDi@dpg-cjrqnegjbais73f9tsvg-a.oregon-postgres.render.com/dogdatabase_0vag`,
-    { logging: false, native: false, ssl:true }
- );
+  `postgres://dogdatabase_0vag_user:qoypINbBwAMs59w2b0Q4glywuKpWbeDi@dpg-cjrqnegjbais73f9tsvg-a.oregon-postgres.render.com/dogdatabase_0vag`,
+  {
+    logging: false,
+    native: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true, // Requiere una conexión SSL/TLS
+      },
+    },
+  }
+);
 
- DogModel(sequelize);
- TemperamentModel(sequelize);
+DogModel(sequelize);
+TemperamentModel(sequelize);
 
- const {dog, temperament} = sequelize.models;
+const { dog, temperament } = sequelize.models;
 
-dog.belongsToMany(temperament, { through: 'dog_temperament' });
-temperament.belongsToMany(dog, { through: 'dog_temperament' });
+dog.belongsToMany(temperament, { through: "dog_temperament" });
+temperament.belongsToMany(dog, { through: "dog_temperament" });
 
- module.exports = {
-    ...sequelize.models,
-    dog,
-    temperament,
-    conn: sequelize,
- }
-
- 
+module.exports = {
+  ...sequelize.models,
+  dog,
+  temperament,
+  conn: sequelize,
+};
